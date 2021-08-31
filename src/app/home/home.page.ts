fe98b69704f96ec9ce4ac45cd7004b0f881428ee
Damian Tarnawsky <damian@ionic.io>
2021-08-31 13:03:15 -0700

    Initial Commit: ion-slides with large first slide

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSegment, IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild('gslides') slides: IonSlides;
+  @ViewChild('segments') segments: IonSegment;
 
+  segment: number = 1;
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+
+  lines: Array<string> = [];
+  constructor() {
+    for (var i = 0; i < 50; i++) {
+      this.lines.push(i.toString());
+    }
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter', this.slides); // this.slides is only available here. ngOnInit it isnt defined
+  }
+
+
+  async onChangeSlide() {
+    const index = await this.slides.getActiveIndex();
+    this.segment = index + 1;
+  }
+
+  segmentChanged(ev: any) {
+    const id = parseInt(ev.detail.value) - 1;
+    this.slides.slideTo(id);
+  }
 }
